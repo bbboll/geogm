63910b965afc59086f05b2b16cf4117f943c7128
Bastian Boll <bastibboll@googlemail.com>
2018-11-16 13:30:51 +0100

    reimplemented blas routines to make dependency optional

--- Makefile
+++ Makefile
@@ -1,10 +1,23 @@
 UNAME_S := $(shell uname -s)
+USE_BLAS := 0
 BLAS_DIR := /usr/local
 
 CXX=clang++
 SRC=src/*.cpp
 STD=c++1z
 
+ifeq ($(USE_BLAS),0)
+blastest: src/blastest.cpp src/blas_wrap.h src/blas_wrap.cpp
+	$(CXX) -lc \
+		   -std=$(STD) \
+		   -o blastest src/blas_wrap.cpp src/blastest.cpp
+optimize: src/optimize.cpp src/graph.cpp src/blas_wrap.h src/blas_wrap.cpp
+	$(CXX) -lc \
+		   -std=$(STD) \
+		   -lboost_iostreams \
+		   -lboost_regex \
+		   -o optimize src/blas_wrap.cpp src/graph.cpp src/optimize.cpp
+else
 ifeq ($(UNAME_S),Linux)
 blastest: src/blastest.cpp src/blas_wrap.h src/blas_wrap.cpp
 	$(CXX) -I$(BLAS_DIR)/include \
@@ -34,6 +47,7 @@ optimize: src/optimize.cpp src/graph.cpp src/blas_wrap.h src/blas_wrap.cpp
 		   -lboost_iostreams \
 		   -lboost_regex \
 		   -o optimize src/blas_wrap.cpp src/graph.cpp src/optimize.cpp
+endif
 endif
 
 read_gz: src/read_gz.cpp
--- src/blas_wrap.cpp
+++ src/blas_wrap.cpp
@@ -3,10 +3,14 @@
 #include <iostream>
 #include <cstring>
 
+#define USE_BLAS 0
+
+#if USE_BLAS
 #ifdef __APPLE__
 	#include <Accelerate/Accelerate.h>
 #else
 	#include <cblas.h>
+#endif
 #endif
 
 /*
@@ -18,6 +22,7 @@ bool almost_equal(double a, double b) {
 
 /*
 	pointwise subdivide by operand
+	without using BLAS system library
 	B ← A / B
 */
 void matrix_psubdivide(const Matrix& A, Matrix& B) {
@@ -36,6 +41,7 @@ void matrix_psubdivide(const Matrix& A, Matrix& B) {
 
 /*
 	pointwise exponential
+	without using BLAS system library
 	A ← exp(A)
 */
 void matrix_pexp(Matrix &A) {
@@ -48,6 +54,7 @@ void matrix_pexp(Matrix &A) {
 
 /*
 	pointwise natural logarithm
+	without using BLAS system library
 	A ← log(A)
 */
 void matrix_plog(Matrix &A) {
@@ -60,6 +67,7 @@ void matrix_plog(Matrix &A) {
 
 /*
 	pointwise power
+	without using BLAS system library
 	A ← A**p
 */
 void matrix_ppower(Matrix &A, const double p) {
@@ -72,6 +80,7 @@ void matrix_ppower(Matrix &A, const double p) {
 
 /*
 	pointwise product
+	without using BLAS system library
 	A ← A*B
 */
 void matrix_pproduct(Matrix &A, const Matrix& B) {
@@ -85,6 +94,7 @@ void matrix_pproduct(Matrix &A, const Matrix& B) {
 
 /*
 	Mean squared error
+	without using BLAS system library
 */
 double matrix_mse(Matrix &A, Matrix &B) {
 	assert( A.m == B.m );
@@ -99,6 +109,8 @@ double matrix_mse(Matrix &A, Matrix &B) {
 	}
 	return sum / (A.m*A.n);
 }
+
+#if USE_BLAS
 
 /*
 	BLAS routine daxpy
@@ -217,6 +229,106 @@ double dot_product(const Matrix& A, const Matrix& B) {
 		1          // B stride
 	);
 }
+
+#else
+
+/*
+	BLAS routine daxpy
+	without using BLAS system library
+	x ← x + αy
+*/
+void matrix_add(Matrix &x, const double alpha, const Matrix&y) {
+	assert( x.m*x.n == y.m*y.n );
+
+	double* dataA = x.raw();
+	double* dataB = y.raw();
+	for (int i = 0; i < x.m*x.n; ++i)
+	{
+		dataA[i] += alpha*dataB[i];
+	}
+}
+
+/*
+	BLAS routine copy
+	without using BLAS system library
+	B ← A
+*/
+void matrix_copy(const Matrix& A, Matrix& B) {
+	assert( A.m == B.m );
+	assert( A.n == B.n );
+
+	double *dataB = B.raw();
+	double *dataA = A.raw();
+	for (int i = 0; i < A.m*A.n; ++i)
+	{
+		dataB[i] = dataA[i];
+	}
+}
+
+/*
+	BLAS routine dgemv
+	without using BLAS system library
+	y ← αAx + βy
+*/
+void matrix_vector_multiply(const Matrix &A, const Matrix &x, Matrix& y, double alpha, double beta) {
+	assert( A.second_dim() == x.m );
+	assert( A.first_dim() == y.m );
+	assert( x.n == 1 );
+	assert( y.n == 1 );
+
+	double* y_raw = y.raw();
+	double* x_raw = x.raw();
+	for (int i = 0; i < A.first_dim(); ++i)
+	{
+		y_raw[i] *= beta;
+		for (int j = 0; j < A.second_dim(); ++j)
+		{
+			y_raw[i] += alpha*A.get(i,j)*x_raw[j];
+		}
+	}
+}
+
+/*
+	BLAS routine dgemm
+	without using BLAS system library
+	C ← αAB + βC
+*/
+void matrix_multiply(const Matrix& A, const Matrix& B, Matrix& C, double alpha, double beta) {
+	assert( A.second_dim() == B.first_dim() );
+	assert( A.first_dim()  == C.m );
+	assert( B.second_dim() == C.n );
+
+	for (int i = 0; i < C.m; ++i)
+	{
+		for (int j = 0; j < C.n; ++j)
+		{
+			C.set(i,j, beta*C.get(i,j) );
+			for (int k = 0; k < A.second_dim(); ++k)
+			{
+				C.set(i,j, C.get(i,j) + alpha*A.get(i,k)*B.get(k,j) );
+			}
+		}
+	}
+}
+
+/*
+	BLAS routine ddot
+	without using BLAS system library
+*/
+double dot_product(const Matrix& A, const Matrix& B) {
+	assert( A.m*A.n == B.m*B.n );
+
+	double* dataA = A.raw();
+	double* dataB = B.raw();
+	double out = 0.0;
+	for (int i = 0; i < A.m*A.n; ++i)
+	{
+		out += dataA[i]*dataB[i];
+	}
+	return out;
+}
+
+#endif
 
 Matrix& Matrix::operator+=(const Matrix& other) {
 	matrix_add(*this, 1.0, other);
